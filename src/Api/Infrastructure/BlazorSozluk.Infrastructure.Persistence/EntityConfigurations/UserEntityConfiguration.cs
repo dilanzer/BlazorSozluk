19b66243a1c3fc4b408b2ded6f5bf4a687380b00
dilanzer <dilanozermail@gmail.com>
2024-04-04 02:49:46 +0300

    proje yapisi ve entity olusturma

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/EntityConfigurations/UserEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.EntityConfigurations
+{
+	public class UserEntityConfiguration : BaseEntityConfiguration<User>
+	{
+		public override void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+		{
+			base.Configure(builder);
+
+			builder.ToTable("users", BlazorSozlukContext.DEFAULT_SCHEMA);
+		}
+	}
+}
